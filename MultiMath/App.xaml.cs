f2456d777a37e08f744e3db69470203230b9524f
franckymougel <francky.mougel@gmail.com>
2022-01-24 20:09:09 -0500

    Creation page calcul

--- MultiMath/App.xaml.cs
+++ MultiMath/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MultiMath.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MultiMath
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new MultiPage();
         }
 
         protected override void OnStart()
